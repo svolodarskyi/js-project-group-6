061f71b6c38c06134b3beed1cddaa8aafd2c5d43
Volodymyr Prudius <vova46772020@gmail.com>
2024-11-22 16:44:06 +0100

    header

--- src/js/header.js
+++ src/js/header.js
@@ -6,24 +6,49 @@ https://medium.com/@haxzie/dark-and-light-theme-switcher-using-css-variables-and
 */
 
 function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
 }
 
 // function to toggle between light and dark theme
 function toggleTheme() {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-light');
-    } else {
-        setTheme('theme-dark');
-    }
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-light');
+  } else {
+    setTheme('theme-dark');
+  }
 }
 
 // Immediately invoked function to set the theme on initial load
 (function () {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-dark');
-    } else {
-        setTheme('theme-light');
-    }
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-dark');
+  } else {
+    setTheme('theme-light');
+  }
 })();
+
+const burgerBtn = document.querySelector('.burger-btn');
+const modal = document.getElementById('modal');
+const modalContainer = modal.querySelector('.modal-container');
+const closeBtn = document.getElementById('close-btn');
+
+// Открытие модалки
+burgerBtn.addEventListener('click', () => {
+  modal.classList.add('active');
+  modalContainer.classList.add('active');
+});
+
+// Закрытие модалки
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('active');
+  modalContainer.classList.remove('active');
+});
+
+// Закрытие при клике вне модалки
+window.addEventListener('click', e => {
+  if (e.target === modal) {
+    modal.classList.remove('active');
+    modalContainer.classList.remove('active');
+  }
+});
