63553d8a21170da9073df2bdf74077d70ad31a18
Volodymyr Prudius <vova46772020@gmail.com>
2024-11-27 01:15:59 +0100

    mobile-menu

--- src/js/header.js
+++ src/js/header.js
@@ -11,33 +11,3 @@ document.getElementById('themeToggle').addEventListener('click', () => {
   const newTheme = currentTheme === 'theme-dark' ? 'theme-light' : 'theme-dark';
   setTheme(newTheme);
 });
-
-const burgerBtn = document.querySelector('.burger-btn');
-const modal = document.getElementById('modal');
-const modalContainer = modal.querySelector('.modal-container');
-const closeBtn = document.getElementById('close-btn');
-
-const mobileLinks = document.querySelectorAll(".modal-link");
-
-mobileLinks.forEach(link =>{
-  link.addEventListener("click", ()=>{
-    modal.classList.remove('active');
-  })
-})
-
-burgerBtn.addEventListener('click', () => {
-  modal.classList.add('active');
-  modalContainer.classList.add('active');
-});
-
-closeBtn.addEventListener('click', () => {
-  modal.classList.remove('active');
-  modalContainer.classList.remove('active');
-});
-
-window.addEventListener('click', e => {
-  if (e.target === modal) {
-    modal.classList.remove('active');
-    modalContainer.classList.remove('active');
-  }
-});
---
+++ src/js/mobil-modal.js
@@ -0,0 +1,29 @@
+const burgerBtn = document.querySelector('.burger-btn');
+const modal = document.getElementById('modal');
+const modalContainer = modal.querySelector('.modal-container');
+const closeBtn = document.getElementById('close-btn');
+
+const mobileLinks = document.querySelectorAll('mobile-menu-link');
+
+mobileLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    modal.classList.remove('active');
+  });
+});
+
+burgerBtn.addEventListener('click', () => {
+  modal.classList.add('active');
+  modalContainer.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('active');
+  modalContainer.classList.remove('active');
+});
+
+window.addEventListener('click', e => {
+  if (e.target === modal) {
+    modal.classList.remove('active');
+    modalContainer.classList.remove('active');
+  }
+});
