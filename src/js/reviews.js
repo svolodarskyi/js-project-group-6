a53b1a9ed0fe6f6cb92d92eddf070c202de52add
Sergii Volodarskyi <47503455+svolodarskyi@users.noreply.github.com>
2024-11-27 00:39:46 -0700

    Update reviews.js

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -57,7 +57,7 @@ const initReviewsSwiper = async () => {
 
   swiper = new Swiper('.swiper', {
     slidesPerView: 1,
-    spaceBetween: 8,
+    spaceBetween: 14,
     keyboard: {
       enabled: true,
     },
